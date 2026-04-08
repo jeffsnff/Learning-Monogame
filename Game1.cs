3924ed96d4272057362f6939b4f9d587c4a6eb24
Jeffrey Seneff <jeffsnff@gmail.com>
2026-04-08 06:35:18 -0400

    Finished Code Quest 4 for Level 4

--- Game1.cs
+++ Game1.cs
@@ -10,9 +10,10 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private SpriteFont _mainFont;
     private Texture2D _heroSprite;
-    private Texture2D _spaceBackgroundSprite;
-    private Texture2D _coinSprite;
-    private Texture2D _crateSprite;
+    private Texture2D _gremlinSprite;
+    private Texture2D _shipBackgroundSprite;
+    private Texture2D _healthVialSprite;
+    private Texture2D _consoleSprite;
 
     public Game1()
     {
@@ -32,10 +33,11 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _mainFont = Content.Load<SpriteFont>("MainFont");
-        _coinSprite = Content.Load<Texture2D>("coin");
+        _healthVialSprite = Content.Load<Texture2D>("healthVial");
         _heroSprite = Content.Load<Texture2D>("hero");
-        _crateSprite = Content.Load<Texture2D>("crate");
-        _spaceBackgroundSprite = Content.Load<Texture2D>("starryBackground");
+        _gremlinSprite = Content.Load<Texture2D>("gremlin");
+        _consoleSprite = Content.Load<Texture2D>("console");
+        _shipBackgroundSprite = Content.Load<Texture2D>("shipBackground");
 
 
         // TODO: use this.Content to load your game content here
@@ -58,15 +60,18 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_spaceBackgroundSprite, new Vector2(0,0), Color.White);
-        // _spriteBatch.DrawString(_mainFont, "Welcome Hero! Do you wish to begin?", new Vector2(100, 100), Color.Red);
+        _spriteBatch.Draw(_shipBackgroundSprite, new Vector2(0,0), Color.White);
+        _spriteBatch.DrawString(_mainFont, "FIGHT!", new Vector2(400, 100), Color.Red);
         
         for(int xCord = 0; xCord<800; xCord = xCord + 64)
         {
-            _spriteBatch.Draw(_crateSprite, new Vector2(xCord, 280), Color.SteelBlue);
+            _spriteBatch.Draw(_consoleSprite, new Vector2(xCord, 280), Color.White);
         }
         
-        _spriteBatch.Draw(_heroSprite, new Vector2(64, 240), Color.SteelBlue);
+        _spriteBatch.Draw(_heroSprite, new Vector2(0,240), Color.White);
+        _spriteBatch.Draw(_gremlinSprite, new Vector2(700, 240), Color.White);
+        
+        _spriteBatch.Draw(_healthVialSprite, new Vector2(380, 240), Color.White);
         
         _spriteBatch.End();
         base.Draw(gameTime);
