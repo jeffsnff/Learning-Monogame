09c65d8ad419242e6ee6f83971f13b8f937eaf39
Jeffrey Seneff <jeffsnff@gmail.com>
2026-04-10 05:31:03 -0400

    Finished Code Quest 5

--- Game1.cs
+++ Game1.cs
@@ -8,12 +8,11 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private SpriteFont _mainFont;
     private Texture2D _heroSprite;
     private Texture2D _gremlinSprite;
     private Texture2D _shipBackgroundSprite;
+    private Texture2D _crateSprite;
     private Texture2D _healthVialSprite;
-    private Texture2D _consoleSprite;
 
     public Game1()
     {
@@ -32,15 +31,14 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _mainFont = Content.Load<SpriteFont>("MainFont");
-        _healthVialSprite = Content.Load<Texture2D>("healthVial");
-        _heroSprite = Content.Load<Texture2D>("hero");
-        _gremlinSprite = Content.Load<Texture2D>("gremlin");
-        _consoleSprite = Content.Load<Texture2D>("console");
-        _shipBackgroundSprite = Content.Load<Texture2D>("shipBackground");
-
 
         // TODO: use this.Content to load your game content here
+        _heroSprite = Content.Load<Texture2D>("hero");
+        _gremlinSprite = Content.Load<Texture2D>("gremlin");
+        _shipBackgroundSprite = Content.Load<Texture2D>("shipBackground");
+        _crateSprite = Content.Load<Texture2D>("crate");
+        _healthVialSprite = Content.Load<Texture2D>("healthVial");
+
     }
 
     protected override void Update(GameTime gameTime)
@@ -59,21 +57,40 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_shipBackgroundSprite, new Vector2(0,0), Color.White);
-        _spriteBatch.DrawString(_mainFont, "FIGHT!", new Vector2(400, 100), Color.Red);
+        // _spriteBatch.Draw(
+        //     _heroSprite, 
+        //     new Vector2(175, 150), 
+        //     null,
+        //     Color.White, 
+        //     0f, 
+        //     new Vector2(0,0),  
+        //     1f, 
+        //     SpriteEffects.None, 
+        //     0f
+        //     );
         
-        for(int xCord = 0; xCord<800; xCord = xCord + 64)
+        _spriteBatch.Draw(
+            _shipBackgroundSprite,
+            new Vector2(-175,-400),
+            null,
+            Color.White,
+            0.67f,
+            new Vector2(0,0),
+            1f,
+            SpriteEffects.None,
+            0f
+            );
+        
+        for (float xCoord = 0; xCoord < 800; xCoord += 64)
         {
-            _spriteBatch.Draw(_consoleSprite, new Vector2(xCord, 280), Color.White);
-        }
+            _spriteBatch.Draw(_crateSprite, new Vector2(xCoord,350), Color.White);
+        } 
         
-        _spriteBatch.Draw(_heroSprite, new Vector2(0,240), Color.White);
-        _spriteBatch.Draw(_gremlinSprite, new Vector2(700, 240), Color.White);
-        
-        _spriteBatch.Draw(_healthVialSprite, new Vector2(380, 240), Color.White);
         
         _spriteBatch.End();
+
         base.Draw(gameTime);
     }
 }
